8c67dc53627cfb41e05ee8c519856dcd5d3e2603
Abhijeet Kumar Singh <40620392+abhijeetkumarsinghofficial@users.noreply.github.com>
2021-10-18 12:51:50 +0530

    Update main.py

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import math
 
+pygame.init()
 screen = pygame.display.set_mode((800, 600))
 # bg
 bg = pygame.image.load('bg.jpg')
@@ -17,13 +18,23 @@ playerX = 370
 playerY = 480
 playerXchange = 0
 
-# enemy
-enemyImg = pygame.image.load('enemy.png')
-enemyImg = pygame.transform.rotate(enemyImg, 180)
-enemyX = random.randint(0, 735)
-enemyY = random.randint(50, 150)
-enemyXchange = 1
-enemyYchange = 20
+# multiple enemy
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyXchange = []
+enemyYchange = []
+num_of_enemy = 3
+
+for i in range(num_of_enemy):
+    # enemy
+    enemyImg.append(pygame.image.load('enemy.png'))
+    # enemyImg=pygame.transform.rotate
+
+    enemyX.append(random.randint(0, 735))
+    enemyY.append(random.randint(50, 150))
+    enemyXchange.append(1)
+    enemyYchange.append(20)
 
 # bullet
 bulletImg = pygame.image.load('bullet.png')
@@ -32,13 +43,24 @@ bulletY = 480
 bulletXchange = 0
 bulletYchange = 5
 bullet_state = "ready"
-score=0
-# score_value = 0
-# font = pygame.font.Font('freesansbold.ttf', 32)
-# textX = 10
-# textY = 10
+
+# scores
+score = 0
+score_value = 0
+font = pygame.font.SysFont('Comic Sans MS', 30)
+textX = 10
+textY = 10
+
+# gameover font
+goverfont = pygame.font.SysFont('Comic Sans MS', 64)
 
 
+def gameover():
+    govertext = goverfont.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(govertext, (200, 250))
+
+
+# score fundtion
 def show_score(x, y):
     score = font.render("Score= " + str(score_value), True, (255, 255, 255))
     screen.blit(score, (x, y))
@@ -49,8 +71,8 @@ def player(playerX, playerY):
     screen.blit(playerImg, (playerX, playerY))
 
 
-def enemy(enemyX, enemyY):
-    screen.blit(enemyImg, (enemyX, enemyY))
+def enemy(enemyX, enemyY, i):
+    screen.blit(enemyImg[i], (enemyX, enemyY))
 
 
 def fire_bullet(x, y):
@@ -92,17 +114,36 @@ while running:
 
     playerX += playerXchange
     enemyX += enemyXchange
+    # HERO MOVEMENT
     if playerX <= 0:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
 
-    if enemyX <= 0:
-        enemyY += enemyYchange
-        enemyXchange = 1
-    elif enemyX >= 736:
-        enemyY += enemyYchange
-        enemyXchange = -1
+    # ENEMY MOVEMENT
+    for i in range(num_of_enemy):
+        # game over
+        if enemyY[i] > 450:
+            for j in range(num_of_enemy):
+                enemyY[j] = 2000
+            gameover()
+            break
+        enemyX[i] += enemyXchange[i]
+        if enemyX[i] <= 0:
+            enemyY[i] += enemyYchange[i]
+            enemyXchange[i] = 1
+        elif enemyX[i] >= 736:
+            enemyY[i] += enemyYchange[i]
+            enemyXchange[i] = -1
+        # collision
+        collision = collide(enemyX[i], enemyY[i], bulletX, bulletY)
+        if collision:
+            bulletY = 400
+            bullet_state = "ready"
+            score_value += 1
+            enemyX[i] = random.randint(0, 735)
+            enemyY[i] = random.randint(50, 150)
+        enemy(enemyX[i], enemyY[i], i)
 
     # Bullet movement
     if bulletY <= 0:
@@ -111,16 +152,9 @@ while running:
     if bullet_state == "fire":
         fire_bullet(bulletX, bulletY)
         bulletY -= bulletYchange
-    # collision
-    collision = collide(enemyX, enemyY, bulletX, bulletY)
-    if collision:
-        bulletY = 400
-        bullet_state = "ready"
-        # score_value += 1
-        enemyX = random.randint(0, 735)
-        enemyY = random.randint(50, 150)
-        print(score)
-    # show_score(textX, textY)
+
+        # print(score)
+    show_score(textX, textY)
     player(playerX, playerY)
-    enemy(enemyX, enemyY)
+
     pygame.display.update()
